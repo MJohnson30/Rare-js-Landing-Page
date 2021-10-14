330daa72ddecf367baa3f768f34804ea86c3409c
Marcus Johnson <Marcus.njohnson64@gmail.com>
2021-10-13 18:07:50 -0700

    Navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+return (
+	<div id="nav">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Collection</a></li>
+		<li><a href="#">Favorites</a></li>
+		<li><a href="#">Login</a></li>
+		<li><a href="#">Contact Us</a></li>
+	</div>
+)};
+
+
+export default Navbar;
+
